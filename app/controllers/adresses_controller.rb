95cfff98464845eb7e555f3732c12c8d9c868a78
Pierre VABRE <pierrevabre@Air-de-Pierre.af83.priv>
2016-10-27 16:39:21 +0200

    adresses index create show

--- app/controllers/adresses_controller.rb
+++ app/controllers/adresses_controller.rb
@@ -2,4 +2,18 @@ class AdressesController < ApplicationController
   def index
   	@adresses = Adresse.all
   end
+
+  def create
+ 	@adresse = Adresse.create title: params[:title]
+ 	redirect_to adress_path @adresse
+  end
+
+  def show
+  	@adresse = Adresse.find(params[:id])
+  end
 end
+
+#adresses_path
+#edit_adress_path + ID
+#new_adress_path
+#adress_path  + ID
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	
-  get 'adresses/index'
+  resources 'adresses', only: [:index, :show, :create, :new]
   get 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
