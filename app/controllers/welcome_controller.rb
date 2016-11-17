a227a50b994e88c1d71994d7a4ddeace5dd7a573
Pierre VABRE <pierrevabre@Air-de-Pierre.af83.priv>
2016-11-17 17:30:32 +0100

    add infowindow gmap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,10 @@ class WelcomeController < ApplicationController
   def home
   	@adresses = Adresse.all
   	@categories = Category.all
+	@hash = Gmaps4rails.build_markers(@adresses) do |adresse, marker|
+	  marker.lat adresse.latitude
+	  marker.lng adresse.longitude
+	  marker.infowindow adresse.title
+	end
   end
 end
